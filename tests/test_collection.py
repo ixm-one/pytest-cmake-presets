8f30d9ff673837b9107a954cd5946c6a39c80311
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2022-12-25 15:45:27 -0800

    ⚗️ More bit bashing
    Signed-off-by: Izzy Muerte <63051+bruxisma@users.noreply.github.com>

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -5,12 +5,12 @@ def test_single(pytester, cmake_project, cmake_presets):
     pytester.maketxtfile(CMakeLists=cmake_project)
     pytester.makefile(".json", CMakePresets=json.dumps(cmake_presets))
 
-    result = pytester.runpytest()
+    result = pytester.runpytest(["--verbose"])
     result.assert_outcomes(passed=1)
 
 
 def test_none(pytester, cmake_project):
     pytester.maketxtfile(CMakeLists=cmake_project)
 
-    result = pytester.runpytest()
+    result = pytester.runpytest(["--verbose"])
     result.assert_outcomes()
